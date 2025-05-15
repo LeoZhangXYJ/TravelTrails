6d26b2e5b03a7646337dd13e662b12984d6daa58
Leo Zhang <1391089202@qq.com>
2025-05-15 15:19:26 +0800

    修改建议

--- user_management/user_models.py
+++ user_management/user_models.py
@@ -15,3 +15,10 @@ class UserUpdate(BaseModel):
 class UserLogin(BaseModel):
     username: str
     password: str
+
+
+######
+#用户模型存储不应该是只有这些东西，还要考虑存储的轨迹数据、各个轨迹点的图片、Blog的数据
+#只是说像作业那样,没说完全一样
+#如何存储用户模型，接口设计，参考前端里的数据是如何存储的，然后想出分析他的接口
+######
